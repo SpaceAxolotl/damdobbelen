188cdce86b840f04f699d4414bbb29ae7e8d5a95
Puzzlewuzzle <baato.chan@gmail.com>
2023-01-15 21:02:40 +0100

    buggy movement update
    yup, it's buggy movement time

--- damdobbelen/Assets/Scripts/CheckersBoard.cs
+++ damdobbelen/Assets/Scripts/CheckersBoard.cs
@@ -10,9 +10,50 @@ public class CheckersBoard : MonoBehaviour
     public Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
     public Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
 
+    private Piece selectedPiece;
+    private Vector2 mouseOver;
+    private Vector2 startDrag;
+    private Vector2 endDrag;
+
+
     private void Start()
     {
-        //GenerateBoard();
+        GenerateBoard();
+    }
+    private void Update()
+    {
+        UpdateMouseOver();
+       
+        //if it is my turn, then select piece.
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+        if (Input.GetMouseButtonDown(0))
+            SelectPiece(x,y);
+            if (Input.GetMouseButtonUp(0))
+                TryMove((int)startDrag.x, (int)startDrag.y, x, y);
+            
+        }
+    }
+    private void UpdateMouseOver()
+    {
+        //if it's my turn, 
+        if(!Camera.main) //als er geen camera is, geef een error msg
+        {
+            Debug.Log("unable to find Main Camera");
+            return;
+        }
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            mouseOver.x = (int)(hit.point.x - boardOffset.x);
+            mouseOver.y = (int)(hit.point.z - boardOffset.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
     }
     private void GenerateBoard()
     {
@@ -52,5 +93,32 @@ public class CheckersBoard : MonoBehaviour
     private void MovePiece(Piece p, int x, int y)
     {
         p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + boardOffset + pieceOffset;
+    }
+
+    private void SelectPiece(int x, int y)
+    {
+        //out of bounds
+        if(x < 0 || x>=pieces.Length || y<0 || y>= pieces.Length)
+            return;
+
+        Piece p = pieces[x, y];
+        if (p != null)
+        {
+            selectedPiece = p;
+            startDrag = mouseOver;
+            Debug.Log(selectedPiece);
+        }
+    }
+
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+        //multiplayer support
+        startDrag = new Vector2(x1, y1);
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = pieces[x1, y1];
+
+        MovePiece(selectedPiece, x2, y2);
+        //check if we are out of bounds
+        //check if 
     }
 }
